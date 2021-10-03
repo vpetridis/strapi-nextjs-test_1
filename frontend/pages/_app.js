15f8d27a136de751c454c1e28ec8b3d58a518423
vpetridis <vpetridis@modulus.gr>
2021-10-03 13:15:31 +0300

    INIT ant-d & basic components & seo

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import Head from "next/head"
+import { createContext } from "react"
+import { getStrapiMedia } from "../lib/media"
+import { fetchAPI } from "../lib/api"
 
+import "../styles/globals.css"
+
+export const GlobalContext = createContext({})
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { global } = pageProps
+  return (
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href={getStrapiMedia(global.favicon)}
+        />
+      </Head>
+      <GlobalContext.Provider value={global}>
+        <Component {...pageProps} />
+      </GlobalContext.Provider>
+    </>
+  )
+}
+
+MyApp.getInitialProps = async (ctx) => {
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(ctx)
+  // Fetch global site settings from Strapi
+  const global = await fetchAPI("/global")
+  // Pass the data to our page via props
+  return { ...appProps, pageProps: { global } }
 }
 
 export default MyApp
