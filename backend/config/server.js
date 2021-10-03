0f9cc35d5c6e4fd26f9ac5918c21689b9f50e9ee
vpetridis <vpetridis@modulus.gr>
2021-10-03 12:25:01 +0300

    INIT strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fbb73290fd979b3425b7fb88d7a24962'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c12e001e-7fb8-4ea4-a102-e3e10624c77f'
+};
